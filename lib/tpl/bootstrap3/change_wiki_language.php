ebc0545ec984856f831642c0a56ef253107c647c
Abrar Burk <burk.abrar@gmail.com>
2019-01-30 10:07:42 +0530

    basiic bug fiixes

--- lib/tpl/bootstrap3/change_wiki_language.php
+++ lib/tpl/bootstrap3/change_wiki_language.php
@@ -11,6 +11,7 @@ include $cwd.'/var_config.php';
 //check URI to see which language space we are in, accordingly put the
 //respective language selection button in active state
 
+
 $compatible_language_codes = array("en", "hi");
 $directoryURI = $_SERVER['REQUEST_URI'];
 $components = explode('=', $directoryURI);
@@ -24,13 +25,24 @@ if($current_language_code == "en"){
   $current_language = NULL;
 }
 
+//detects if its login page, in which case, it doesn't display the language selection option.
+// honestly im not sure if this is necessary
+
+// $this_is_login = false;
+// $detect_login_pg = explode('?', $directoryURI);
+// if(strpos($detect_login_pg[1], "login") !== false){
+//   $this_is_login = true;
+// } else{
+//   $this_is_login = false;
+// };
 
 
 ?>
 
-
+<!-- the php on the <ul> is an extension of the logic for display/hide lang selection based on login page -->
 
 <ul class="nav navbar-nav" id = "linklist">
+
         <li class="<?php if ($current_language==$english) {echo "active"; } else  {echo "noactive";}?>">
           <a href="<?php if ($current_language==$english) {echo "#"; } else  {echo "/".$root_folder."/doku.php?id=".$english.":start";}?>">English</a>
         </li>
